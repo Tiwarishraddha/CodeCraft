890300cfd12f4264c420eec0a57f73d8e104e10f
Tiwarishraddha <shraddhatiwari2406@gmail.com>
2025-01-28 15:12:20 +0530

    Fix bug in App.jsx

--- my-codecraft-app/src/components/App.jsx
+++ my-codecraft-app/src/components/App.jsx
@@ -50,7 +50,6 @@ function App() {
           srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
-          frameBorder="0"
           width="100%"
           height="100%"
         />
